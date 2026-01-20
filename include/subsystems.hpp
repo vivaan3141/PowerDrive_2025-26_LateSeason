d0b76e0e5a7f8f3e453c33ee5f7c162292251ec4
vivaan3141 <vivaangupta3141@gmail.com>
2026-01-19 17:48:28 -0800

    Auton edits

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -7,7 +7,7 @@
 extern Drive chassis;
 // Your motors, sensors, etc. should go here.  Below are examples
 
-inline pros::MotorGroup intake(-9, 21);
+inline pros::MotorGroup intake({-9, 21});
 inline pros::Motor scorer(17);
 
 
