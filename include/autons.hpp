6f5ca77e78a3ec9fe031556ad22dd9939e7200fe
vivaan3141 <vivaangupta3141@gmail.com>
2026-01-21 17:28:07 -0800

    Refractor

--- include/autons.hpp
+++ include/autons.hpp
@@ -1,26 +1,14 @@
 #pragma once
-
 // Init()
-
 void default_constants();
-
 // Worst case
-
 void middleGoal();
-
 // Elim
-
 void elimLongGoalLeft();
-
 void elimLongGoalRight();
-
 // Control Zone
 void controlZoneLeftLongGoal();
-
 void controlZoneRightLongGoal();
-
 // BIGGIES
-
 void soloAWP();
-
 void skills();
